f2ea7380d00c30aa07c3863512d9f3f6ac4781dd
25slivingston <25slivingston@derryfield.org>
2023-11-12 00:44:57 -0500

    finished HW07
    yes i finished it

--- Unit03/HW07partA/HW07AT.java
+++ Unit03/HW07partA/HW07AT.java
@@ -8,7 +8,7 @@ public class HW07AT {
 
         Scanner input = new Scanner(System.in);
 
-        String numerals = "your numeral is: " 
+        String numerals = "your numeral is: ";
         int number = 0;
 
         while (number > 3999 || number < 1) {
@@ -20,50 +20,102 @@ public class HW07AT {
 
         while (number >= 1000) {
             //add M 
-            numerals = 
+            String M = "M";
+            numerals = numerals.concat(M);
 
             number = number - 1000;
         }
         
         while (number >= 900) {
             //add CM
+            String CM = "CM";
+            numerals =numerals.concat(CM);
             number = number - 900;
         }
 
         while (number >= 500) {
             // add D
-            number = number - 100;
+            String D = "D";
+            numerals =numerals.concat(D);
+            number = number - 500;
+        }
+
+        while (number >= 400) {
+            // add CD
+            String CD = "CD";
+            numerals =numerals.concat(CD);
+            number = number - 400;
         }
 
         while (number >= 100) {
             // add C
+            String C = "C";
+            numerals =numerals.concat(C);
             number = number - 100;
         }
 
         while (number >= 90) {
             // add XC
+            String XC = "XC";
+            numerals =numerals.concat(XC);
             number = number - 90;
         }
 
         while (number >= 50) {
             // add L
-            number = number - 10;
+            String L = "L";
+            numerals =numerals.concat(L);
+            number = number - 50;
+        }
+
+        while (number >= 40) {
+            // add XL
+            String XL = "XL";
+            numerals =numerals.concat(XL);
+            number = number - 40;
         }
 
         while (number >= 10) {
             // add X
+            String X = "X";
+            numerals =numerals.concat(X);
             number = number - 10;
         }
 
         while (number >= 9) {
             // add IX
+            String IX = "IX";
+            numerals =numerals.concat(IX);
             number = number - 9;
         }
 
-        System.out.println("This is your roman numeral: " + number);
+        while (number >= 5) {
+            // add V
+            String V = "V";
+            numerals =numerals.concat(V);
+            number = number - 5;
+        }
+
+        while (number >= 4) {
+            // add IV
+            String IV = "IV";
+            numerals =numerals.concat(IV);
+            number = number - 4;
+        }
+
+        while (number >= 1) {
+            // add I
+            String I = "I";
+            numerals =numerals.concat(I);
+            number = number - 1;
+        }
+
+        System.out.println("This is your roman numeral: " + numerals);
 
 
-
+  String s1="Sachin ";  
+   String s2="Tendulkar";  
+   String s3=s1.concat(s2);  
 
         
 
