dfdcceb8658370834744db8ae743c7979d8a55e4
25slivingston <25slivingston@derryfield.org>
2023-12-04 13:46:32 -0500

    added .gitignore

--- Unit05/ATHW08/ATHW08.java
+++ Unit05/ATHW08/ATHW08.java
@@ -3,10 +3,18 @@ class Die {
     private int faceValue; // outside the class cannot acsess this private
 
     //constructors
+    //this is the first form, that takes no arguments.
+    //now you can choose between the two starting points
     public Die(){
         //initialise the die
         faceValue = 1;
 
+    }
+    //create a second constructor that takes an interger as an argument
+    //this is the second form of the constructor that takes a value
+    //now you can choose between the two starting points
+    public Die(int newValue) {
+        faceValue = newValue;
     }
 
 
@@ -34,6 +42,11 @@ public class ATHW08 {
         Die die = new Die();
         int answer = die.getFaceValue(); //have to put this into the print statement
         System.out.println("the die value is equal to " + answer);
+
+        //change the value of the die
+        die.setFaceValue(6);
+        int answer2 = die.getFaceValue();
+        System.out.println("the die value is equal to " + answer2);
 
     }
 }
